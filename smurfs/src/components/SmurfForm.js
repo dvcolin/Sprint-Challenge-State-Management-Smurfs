3c2e0ff69716c64afbc559c0443c5e0158e70911
dvcolin <46186956+dvcolin@users.noreply.github.com>
2019-08-16 10:10:28 -0700

    converted SmurfForm to formik form

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react'
 import styled from 'styled-components'
 import { connect } from 'react-redux'
 import {addSmurf} from '../actions'
+import { Form, Field, withFormik } from 'formik'
 
-const StyledForm = styled.form`
+const StyledForm = styled(Form)`
     display: flex;
     flex-direction: column;
     align-items: center;
     margin: 1.5rem 0;
 `
 
-const StyledInput = styled.input`
+const StyledInput = styled(Field)`
     margin: 1.5rem 0;
 `
 const SubmitButton = styled.button`
@@ -18,28 +19,14 @@ const SubmitButton = styled.button`
 `
 
 const SmurfForm = props => {
-
-    const [info, setInfo] = useState({
-        name: '',
-        age: '',
-        height: ''
-    })
-
-    const handleChanges = e => {
-        setInfo({...info, [e.target.name]: e.target.value})
-    }
-
-
     return (
         <StyledForm>
             <h1>Add a smurf</h1>
-            <StyledInput onChange={handleChanges} type='text' name='name' placeholder='Enter name' />
-            <StyledInput onChange={handleChanges} type='text' name='age' placeholder='Enter age' />
-            <StyledInput onChange={handleChanges} type='text' name='height' placeholder='Enter height' />
+            <StyledInput type='text' name='name' placeholder='Enter name' />
+            <StyledInput type='text' name='age' placeholder='Enter age' />
+            <StyledInput type='text' name='height' placeholder='Enter height' />
 
-            <SubmitButton onClick={e => {
-                e.preventDefault();
-                props.addSmurf(info)}} type='submit'>Submit</SubmitButton>
+            <SubmitButton type='submit'>Submit</SubmitButton>
 
             
         </StyledForm>
@@ -47,4 +34,20 @@ const SmurfForm = props => {
 }
 
 
-export default connect(null, { addSmurf })(SmurfForm)
+const FormikForm = withFormik({
+    mapPropsToValues(values) {
+        return {
+            name: values.name || '',
+            age: values.age || '',
+            height: values.height || ''
+        };
+    },
+
+    handleSubmit(values, { props, resetForm }) {
+        props.addSmurf(values);
+        resetForm();
+    }
+})(SmurfForm)
+
+
+export default connect(null, { addSmurf })(FormikForm)
